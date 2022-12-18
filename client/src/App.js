8e2d4eed7dc4f10967b5c57b704ddc2df69cb000
Borislav Stefanov <prohaies3101@gmail.com>
2022-12-18 22:34:39 +0200

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,20 @@
+import React from "react";
+import "./App.css";
+import "./index.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./components/Home/Home/Home";
+import SideBar from "./components/Sidebar/Sidebar";
+
+function App() {
+  return (
+    <Router>
+ <SideBar>
+        <Routes>
+          <Route path="/" element={<Home/>} />
+        </Routes>
+        </SideBar>
+    </Router>
+  );
+}
+
+export default App;
---
+++ client/src/components/Home/Home/Home.jsx
@@ -0,0 +1,32 @@
+import React, { useState, useEffect } from 'react';
+import { Container,  Grow, Grid } from '@material-ui/core';
+import { useDispatch } from 'react-redux';
+import Posts from '../Posts/Posts';
+import { getPosts } from '../../../actions/posts';
+
+
+const Home = () => {
+  const [currentId, setCurrentId] = useState(0);
+  const dispatch = useDispatch();
+
+
+  useEffect(() => {
+    dispatch(getPosts());
+  }, [currentId, dispatch]);
+
+  return (
+    <Container maxWidth="lg">
+      
+      <Grow in>
+        <Container>
+          <Grid container justify="space-between" alignItems="stretch" spacing={3}>
+            <Grid item xs={12} sm={7}>
+              <Posts setCurrentId={setCurrentId} />
+            </Grid>
+          </Grid>
+        </Container>
+      </Grow>
+    </Container>
+  );
+};
+export default Home
