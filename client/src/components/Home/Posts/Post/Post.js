acea996dbb7b76ffdd3a13a27ab88c6ba8fb10f5
Borislav Stefanov <prohaies3101@gmail.com>
2023-01-19 16:59:52 +0200

    fasterLikes

--- client/src/components/Home/Posts/Post/Post.js
+++ client/src/components/Home/Posts/Post/Post.js
@@ -30,7 +30,7 @@ const Post = ({ post, setCurrentId }) => {
     }
   };
   const Likes = () => {
-    if (post.likes.length > 0) {
+    if (likes.length > 0) {
       return post.likes.find((like) => like === userId)
         ? (
           <><ThumbUpAltIcon fontSize="small" />&nbsp;{post.likes.length > 2 ? `You and ${post.likes.length - 1} others` : `${post.likes.length} like${post.likes.length > 1 ? 's' : ''}` }</>
